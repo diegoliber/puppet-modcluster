38527969c7ef8da74876e5d820adfaa30125d203
Jairo Junior <jairo.sjunior@dataprev.gov.br>
2015-04-02 23:24:23 -0300

    Initial import.

---
+++ README.md
@@ -0,0 +1,21 @@
+## Puppet modcluster module
+
+Installs modcluster in an existing apache installation
+
+Uses puppetlabs-apache
+
+**Sample**:
+
+    class { 'apache': }
+
+    class { 'modcluster':
+      download_url            => 'http://downloads.jboss.org/mod_cluster//1.2.6.Final/linux-x86_64/mod_cluster-1.2.6.Final-linux2-x64-so.tar.gz',
+      listen_ip_address       => '*',
+      allowed_network         => '10.2.255',
+      balancer_name           => 'mybalancer',
+      manager_allowed_network => '10.2.255'
+    }
+
+    Class['apache'] ->
+      Class['modcluster'] ~>
+        Service['httpd']
---
+++ manifests/init.pp
@@ -0,0 +1,39 @@
+class modcluster(
+  $port = '6666',
+  $listen_ip_address = '127.0.0.1',
+  $allowed_network = '127.0.0.1',
+  $balancer_name = 'mybalancer',
+  $manager_allowed_network = '127.0.0.1',
+  $download_url = undef,
+) {
+
+  #improve
+  $file_name = 'modcluster.tar.gz'
+
+  wget::fetch { "Download modcluster ${download_url}":
+    source      => $download_url,
+    destination => "/opt/${file_name}",
+    cache_dir   => '/var/cache/wget',
+    cache_file  => $file_name,
+    notify      => Exec['Extract modcluster']
+  }
+
+  exec { 'Extract modcluster':
+    command     => "tar -xzf /opt/${file_name}",
+    cwd         => '/etc/httpd/modules',
+    creates     => "/etc/httpd/modules/mod_proxy_cluster.so",
+    group       => 'root',
+    user        => 'root',
+    path        => ['/usr/bin', '/usr/sbin', '/bin', '/sbin']
+  }
+
+  file { '/etc/httpd/conf.d/modcluster.conf':
+    ensure  => file,
+    owner   => 'root',
+    group   => 'root',
+    content => template('modcluster/modcluster.conf.erb'),
+    mode    => '0755',
+    require => Exec['Extract modcluster']
+  }
+
+}
